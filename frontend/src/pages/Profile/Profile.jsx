3744efa0a27ff9d405343270b68ea68be7a1c0d2
Bruno Felipe <brunofelipehp8@gmail.com>
2023-07-04 19:10:31 -0300

    Displaying user photos

--- frontend/src/pages/Profile/Profile.jsx
+++ frontend/src/pages/Profile/Profile.jsx
@@ -1,9 +1,17 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
+import { Message } from "../../components/Message";
 import { getUserDetails } from "../../slices/userSlice";
 import { uploads } from "../../utils/config";
+
 import "./Profile.css";
+
+import {
+  getUserPhotos,
+  publishPhoto,
+  resetMessage,
+} from "../../slices/photoSlice";
 
 export const Profile = () => {
   const { id } = useParams();
@@ -12,16 +20,53 @@ export const Profile = () => {
 
   const { user, loading } = useSelector((state) => state.user);
   const { user: useAuth } = useSelector((state) => state.auth);
+  const {
+    photos,
+    loading: loadingPhoto,
+    message: messagePhoto,
+    error: errorPhoto,
+  } = useSelector((state) => state.photo);
+
+  const [title, setTitle] = useState("");
+  const [image, setImage] = useState("");
 
   const newPhotoForm = useRef();
   const editPhotoForm = useRef();
 
   useEffect(() => {
     dispatch(getUserDetails(id));
+    dispatch(getUserPhotos(id));
   }, [dispatch, id]);
+
+  const handleFile = (e) => {
+    const image = e.target.files[0];
+
+    setImage(image);
+  };
 
   const submitHandle = (e) => {
     e.preventDefault();
+
+    const photoData = {
+      title,
+      image,
+    };
+
+    const formData = new FormData();
+
+    const photoFormData = Object.keys(photoData).forEach((key) =>
+      formData.append(key, photoData[key])
+    );
+
+    formData.append("photo", photoFormData);
+
+    dispatch(publishPhoto(formData));
+
+    setTitle("");
+
+    setTimeout(() => {
+      dispatch(resetMessage());
+    }, 2000);
   };
 
   if (loading) {
@@ -46,17 +91,49 @@ export const Profile = () => {
             <form onSubmit={submitHandle}>
               <label>
                 <span>Título para a foto:</span>
-                <input type="text" placeholder="Insira um título" />
+                <input
+                  type="text"
+                  placeholder="Insira um título"
+                  onChange={(e) => setTitle(e.target.value)}
+                  value={title || ""}
+                />
               </label>
               <label>
                 <span>Imagem:</span>
-                <input type="file" />
+                <input type="file" onChange={handleFile} />
               </label>
-              <input type="submit" value="Postar" />
+              {!loadingPhoto && <input type="submit" value="Postar" />}
+              {loadingPhoto && (
+                <input type="submit" disabled value="Aguarde..." />
+              )}
             </form>
           </div>
+          {errorPhoto && <Message msg={errorPhoto} type="error" />}
+          {messagePhoto && <Message msg={messagePhoto} type="success" />}
         </>
       )}
+      <div className="user-photos">
+        <h2>Fotos publicadas:</h2>
+        <div className="photos-container">
+          {photos &&
+            photos.map((photo) => (
+              <div className="photo" key={photo._id}>
+                {photo.image && (
+                  <img
+                    src={`${uploads}/photos/${photo.image}`}
+                    alt={photo.title}
+                  />
+                )}
+                {id === useAuth._id ? (
+                  <p>actions</p>
+                ) : (
+                  <Link className="btn" to={`/photos/${photo._id}`}></Link>
+                )}
+              </div>
+            ))}
+          {photos.length === 0 && <p>Ainda não há fotos publicadas</p>}
+        </div>
+      </div>
     </div>
   );
 };
